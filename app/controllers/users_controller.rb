8009a3ff95abcbed4d715f4e89e309cf930d6e72
Lucia Barrios Chavez <abarrios.chavez+wrkshp@gmail.com>
2021-06-02 22:31:11 -0600

    User show specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
 
-    # def create
-    #     user = User.new(user_params)
-    #     if user.save
-    #     render json: user, status: 201, location: [:api, user]
-    #     else
-    #     render json: { errors: user.errors }, status: 422
-    #     end
-    # end 
+    render json: @user.to_json
+  end
+
+  # def create
+  #     user = User.new(user_params)
+  #     if user.save
+  #     render json: user, status: 201, location: [:api, user]
+  #     else
+  #     render json: { errors: user.errors }, status: 422
+  #     end
+  # end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,23 +1,31 @@
 require 'spec_helper'
-require "rails_helper"
-require "pry"
-describe UsersController do
+require 'rails_helper'
 
+describe UsersController do
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace" }
 
-  describe "GET #show" do
-    before(:each) do
-      @user = FactoryBot.create :user
-      subject { get :show, params: {id: @user.id} }
+  describe 'GET #show' do
+    let!(:user) { FactoryBot.create(:user) }
+    let(:user_id) { user.id }
+
+    before { get :show, params: { id: user.id } }
+
+    context 'when the response is successful' do
+      it 'returns the information about a reporter on a hash' do
+        expect(JSON.parse(response.body)['email']).to eq(user.email)
+      end
+
+      it 'returns successful code' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    # it "returns the information about a reporter on a hash" do
-    #   binding.pry  
-    #   user_response = JSON.parse(response.body, symbolize_names: true)
-    #   expect(user_response[:email]).to eql @user.email
-    # end
+    context 'when the user does not exists' do
+      let(:user_id) { 100 }
 
-    it { expect(response).to have_http_status(200) }
- end
-
+      it 'returns status not_found' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
